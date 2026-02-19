96fdece567e36098897542b5a8290b543930373d
MiniKlar <omontloris@gmail.com>
2026-02-19 04:10:37 +0100

    - Added HeaderResponse class for managing HTTP response headers

---
+++ HeaderResponse.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HeaderResponse.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 04:02:28 by lomont            #+#    #+#             */
+/*   Updated: 2026/02/19 04:03:26 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HeaderResponse.hpp"
+
+HeaderResponse::HeaderResponse(/* args */)
+{
+}
+
+HeaderResponse::~HeaderResponse()
+{
+}
---
+++ HeaderResponse.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HeaderResponse.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/19 04:02:32 by lomont            #+#    #+#             */
+/*   Updated: 2026/02/19 04:03:17 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef HEADERRESPONSE_HPP
+#define HEADERRESPONSE_HPP
+
+class HeaderResponse
+{
+	private:
+		/* data */
+	public:
+		HeaderResponse(/* args */);
+		~HeaderResponse();
+};
+
+#endif
