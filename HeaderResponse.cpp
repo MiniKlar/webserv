2e00530899f9d55b9017e7bf6f839e0425193eed
MiniKlar <omontloris@gmail.com>
2026-03-03 21:54:47 +0100

    - Refactored HeaderResponse class to enhance HTTP response handling and add support for various status codes

--- HeaderResponse.cpp
+++ HeaderResponse.cpp
@@ -6,19 +6,82 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 04:02:28 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/21 04:55:45 by lomont           ###   ########.fr       */
+/*   Updated: 2026/03/03 02:28:57 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HeaderResponse.hpp"
 
-HeaderResponse::HeaderResponse(HeaderRequest& _request) : _request(_request) {
+HeaderResponse::HeaderResponse(HeaderRequest& setRequest) : request(setRequest) {
+	char*	bufferFileRequested;
+
+	std::cout << request.GetMethod();
+	if (request.GetMethod() == POST) {
+		std::cout << "tu es ici" << std::endl;
+		code = CREATED;
+		std::string pathfile = "./html/drive/image1.png";
+		getContentLength(pathfile);
+	}
+	else {
+		std::string pathfile = FILE_LOCATION + request.GetRequestTarget();
+		getContentLength(pathfile);
+		bufferFileRequested = SearchFileRequested(pathfile);
+		if (!bufferFileRequested)
+			ft_error("[CRASH] buffer empty", 28);
+		content = std::string(bufferFileRequested, fileSize);
+		delete[] bufferFileRequested;
+	}
 	return;
 }
 
-HeaderResponse::~HeaderResponse(void)
-{
+HeaderResponse::HeaderResponse(void) {
 	return;
+}
+
+HeaderResponse::HeaderResponse(const HeaderResponse& other) {
+	*this = other;
+	return ;
+}
+
+HeaderResponse& HeaderResponse::operator=(const HeaderResponse& other) {
+	if (&other != this) {
+		this->content = other.content;
+		this->fileSize = other.fileSize;
+		this->Map = other.Map;
+		this->stringFileSize = other.stringFileSize;
+		this->code = other.code;
+	}
+	return (*this);
+}
+
+HeaderResponse::~HeaderResponse(void) {
+	return;
+}
+
+char* HeaderResponse::SearchFileRequested(std::string& pathfile) {
+	int		file;
+	int		bread;
+	char 	*buffer;
+
+	buffer = NULL;
+	file = open(pathfile.c_str(), O_RDONLY);
+	if (file == -1)
+		ft_error("[CRASH] can't open", 21);
+	buffer = new char[fileSize + 1];
+	if (!buffer)
+		ft_error("Memory allocation failed", 20);
+	bread = read(file, buffer, fileSize);
+	if (bread <= 0) {
+		delete[] buffer;
+		ft_error("read failed", 21);
+	}
+	buffer[bread] = '\0';
+	close(file);
+	return (buffer);
+}
+
+std::string HeaderResponse::getContent(void) {
+	return (this->content);
 }
 
 std::string HeaderResponse::getCurrentTime( void ) {
@@ -27,45 +90,92 @@ std::string HeaderResponse::getCurrentTime( void ) {
 	char ptr[1024];
 	currentTime = time(NULL);
 	localTime = localtime(&currentTime);
-	if (strftime(ptr, 1024, "%a, %d %b %G %T %Z", localTime) == 0) {
+	if (strftime(ptr, 1024, "%a, %d %b %Y %X %Z", localTime) == 0) {
 		std::cerr << "error time" << std::endl;
 		exit(15);
 	}
-	std::cout << ptr << std::endl;
 	return (std::string(ptr));
 }
 
-std::string HeaderResponse::getContentLength(void) {
+void	HeaderResponse::getContentLength(std::string& pathfile) {
 	struct stat s;
-	char ptr[1024];
-	if (stat("test.html", &s) == -1) {
-		std::cerr << "error stat" << std::endl;
-		exit(16);
+	std::ostringstream oss;
+	std::string size;
+	if (stat(pathfile.c_str(), &s) == -1) {
+		if (stat(PAGE_404, &s) == -1)
+			ft_error("[CRASH] PAGE_404 stat failed", 16);
+		code = NOT_FOUND;
+		pathfile = PAGE_404;
 	}
-	if (sprintf(ptr, "%lld", s.st_size) < 0)
-	{
-		std::cerr << "error stat" << std::endl;
-		exit(17);
-	}
-	return (std::string(ptr));
+	if (request.GetMethod() == GET && pathfile != PAGE_404)
+		code = OK;
+	fileSize = s.st_size;
+	oss << s.st_size;
+	stringFileSize = oss.str();
 }
 
 std::string HeaderResponse::getConnectionStatut(void) {
-	std::vector<std::pair<std::string, std::string> >& map = this->_request.getPairs();
-	std::vector<std::pair<std::string, std::string> >::iterator it;
-	it = std::find_if(map.begin(), map.end(), [](const std::pair<std::string, std::string>& p) {return p.first == "Connection:";});
+	std::map<std::string, std::string>& map = Map;
+	std::map<std::string, std::string>::iterator it;
+	it = Map.find("Connection:");
 	if (it != map.end())
-		return (it->first + " " + it->second);
+		return ("Connection: " + it->second);
 	return ("");
 }
 
 std::string HeaderResponse::code_200( void ) {
-	return ("HTTP/1.1 200 OK\r\nDate: " + this->getCurrentTime() + "\r\nServer: Webserv\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: " + getContentLength() + "\r\n" + getConnectionStatut() + "\r\n");
+	return ("HTTP/1.1 200 OK\r\nDate: "
+	+ this->getCurrentTime() + "\r\nServer: Webserv\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: "
+	+ stringFileSize + "\r\n" + getConnectionStatut() + "\r\n\r\n");
 }
+
+std::string HeaderResponse::code_404(void) {
+	return ("HTTP/1.1 404 Not Found\r\nDate: "
+	+ this->getCurrentTime() + "\r\nServer: Webserv\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: "
+	+ stringFileSize + "\r\n" + getConnectionStatut() + "\r\n\r\n");
+}
+
+std::string HeaderResponse::code_201(void) {
+	return ("HTTP/1.1 201 Created\r\nDate: "
+	+ this->getCurrentTime() + "\r\nServer: Webserv\r\nLocation: " + getLocation() + "Content-Type: application/json\r\nContent-Length: "
+	+ stringFileSize + "\r\n\r\n");
+}
+
+std::string HeaderResponse::code_204(void) {
+	return ("HTTP/1.1 204 No Content\r\nDate: " + this->getCurrentTime() + "\r\nServer: Webserv\r\n" + getConnectionStatut() + "\r\n\r\n");
+}
+
+std::string HeaderResponse::getLocation(void) {
+	std::string filepath = UPLOAD_LOCATION + request.GetPathImageCreated();
+	if (access(filepath.c_str(), R_OK) == 0)
+		return (filepath);
+	else
+		return ("error"); //better define the error management of access
+}
+
+off_t& HeaderResponse::GetFileSize(void) {
+	return (fileSize);
+}
+
+Code HeaderResponse::GetCode(void) {
+	return (code);
+}
+
+// HTTP/1.1 201 Created
+// Date: Wed, 18 Feb 2026 22:01:00 GMT
+// Server: nginx/1.18.0
+// Location: /api/users/43
+// Content-Type: application/json
+// Content-Length: 51
 
 // HTTP/1.1 200 OK
 // Date: Wed, 18 Feb 2026 22:00:00 GMT
 // Server: Apache/2.4.41 (Ubuntu)
 // Content-Type: text/html; charset=UTF-8
 // Content-Length: 138
+// Connection: keep-alive
+
+// HTTP/1.1 204 No Content
+// Date: Wed, 18 Feb 2026 22:02:00 GMT
+// Server: nginx/1.18.0
 // Connection: keep-alive
--- HeaderResponse.hpp
+++ HeaderResponse.hpp
@@ -6,34 +6,65 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 04:02:32 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/21 04:09:55 by lomont           ###   ########.fr       */
+/*   Updated: 2026/03/03 00:27:27 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
 #ifndef HEADERRESPONSE_HPP
 #define HEADERRESPONSE_HPP
+
+#define PAGE_404 "./html/error.html"
+#define UPLOAD_LOCATION "./html/drive/"
+#define	FILE_LOCATION "./html"
 
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
 #include <sys/stat.h>
 #include <cstdio>
-
+#include <map>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
 #include "HeaderRequest.hpp"
+#include "utils.hpp"
+
+enum	Code {
+	OK = 200,
+	CREATED = 201,
+	NO_CONTENT = 204,
+	NOT_FOUND = 404
+};
 
 class HeaderResponse
 {
 	private:
-		HeaderRequest& _request;
+		HeaderRequest	request;
+		std::map<std::string, std::string>	Map;
+		off_t			fileSize;
+		std::string		stringFileSize;
+		std::string		content;
+		enum Code		code;
 	public:
-		HeaderResponse(HeaderRequest& _request);
+		HeaderResponse(void);
+		HeaderResponse(const HeaderResponse& other);
+		HeaderResponse(HeaderRequest&);
+		HeaderResponse& operator=(const HeaderResponse& other);
 		~HeaderResponse(void);
+		char*		SearchFileRequested(std::string&);
 		std::string getCurrentTime(void);
 		std::string code_200(void);
-		std::string getContentLength(void);
+		std::string code_201(void);
+		std::string code_204(void);
+		std::string code_404(void);
+
+		//getters
+		void		getContentLength(std::string&); //setter in reality
+		std::string getContent(void);
 		std::string getConnectionStatut(void);
+		std::string getLocation(void);
+		off_t& GetFileSize(void);
+		Code GetCode(void);
 };
 
 #endif
