461f150ccb50e07f0e7cced8dbcd606429e6a002
MiniKlar <omontloris@gmail.com>
2026-03-03 21:55:49 +0100

    - Implemented server class with constructors, destructors, and socket management

--- webserv.cpp
+++ webserv.cpp
@@ -6,8 +6,63 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 23:17:19 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/19 04:01:43 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/26 21:55:44 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
+
+//Constructor server
+server::server(void) : f(getprotobyname("TCP")) {
+	const int opt = 1;
+	logs("Web server started");
+	if (!this->f)
+		ft_error("getprotobyname failed", 1);
+	fillSockaddrStruct();
+	ServerSocket = socket(PF_INET, SOCK_STREAM, f->p_proto);
+	if (ServerSocket == -1)
+		ft_error("socket failed", 2);
+	if (setsockopt(ServerSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+		ft_error("setsockopt failed", 3);
+	BindServerSocket = bind(ServerSocket, (struct sockaddr*)&sa, sizeof(sa));
+	if (BindServerSocket == -1)
+		ft_error("bind failed", 4);
+	ListenServerSocket = listen(ServerSocket, 1);
+	if (ListenServerSocket == -1)
+		ft_error("listen creation failed", 5);
+	fcntl(ServerSocket, F_SETFL, O_NONBLOCK);
+	EvenementQueue = kqueue();
+	if (EvenementQueue == -1)
+		ft_error("kqueue fd creation failed", 6);
+	EV_SET(&event, ServerSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
+	if (kevent(EvenementQueue, &event, 1, NULL, 0, NULL) == -1)
+		ft_error("kevent event addition failed", 7);
+}
+
+//Destruct server
+server::~server(void) {
+	return ;
+}
+
+struct kevent* server::getTevent(void) {
+	return (&this->tevent);
+}
+
+struct kevent* server::getevent(void) {
+	return (&this->event);
+}
+
+int& server::getServerSocket(void) {
+	return (this->ServerSocket);
+}
+
+int& server::getEvenementQueue(void) {
+	return (this->EvenementQueue);
+}
+
+void server::fillSockaddrStruct(void) {
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(80);
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);
+}
--- webserv.hpp
+++ webserv.hpp
@@ -6,11 +6,9 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/15 23:09:55 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/21 04:20:31 by lomont           ###   ########.fr       */
+/*   Updated: 2026/03/02 00:39:05 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#pragma once
 
 #ifndef WEBSERV_HPP
 #define WEBSERV_HPP
@@ -27,10 +25,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <iostream>
+#include <map>
+#include "utils.hpp"
 
-#include "HeaderRequest.hpp"
-#include "HeaderResponse.hpp"
+class server
+{
+	private:
+		struct sockaddr_in 	sa;
+		protoent* 			f;
+		struct kevent 		event;
+		struct kevent 		tevent;
+		int 				ServerSocket;
+		int 				BindServerSocket;
+		int 				ListenServerSocket;
+		//int 				AcceptClientConnexion;
+		int					EvenementQueue;
+		void fillSockaddrStruct(void);
+	public:
+		//Constructors / Destructors
+		server(void);
+		~server(void);
+		//getters
+		struct kevent* getevent(void);
+		struct kevent* getTevent(void);
+		int&	getServerSocket(void);
+		int&	getEvenementQueue(void);
+};
 
 extern int errno;
 
 #endif
+
