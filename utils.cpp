4db8e32ff28ad412f753a68c8815174cf322b207
MiniKlar <omontloris@gmail.com>
2026-03-07 03:39:12 +0100

    - Improved error logging in ft_error function for better crash diagnostics

--- utils.cpp
+++ utils.cpp
@@ -6,16 +6,14 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 21:15:00 by lomont            #+#    #+#             */
-/*   Updated: 2026/03/02 23:43:07 by lomont           ###   ########.fr       */
+/*   Updated: 2026/03/03 23:47:56 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.hpp"
 
 void	ft_error(const std::string& str, int exit_code) {
-	std::cerr << str << std::endl;
-	std::cerr << strerror(errno) << std::endl;
-	std::cout << errno << std::endl;
+	std::cerr << "[CRASH]" << str << strerror(errno) << std::endl;;
 	exit(exit_code);
 }
 
