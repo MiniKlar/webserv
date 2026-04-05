f1837a1515b106c3a1ac31032c5602a83224a033
MiniKlar <omontloris@gmail.com>
2026-04-05 22:26:59 +0200

    - Refactored utility functions: updated error and logging functions for consistency, added error messages, and improved code structure.

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,26 +6,36 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 21:15:00 by lomont            #+#    #+#             */
-/*   Updated: 2026/03/08 14:35:38 by lomont           ###   ########.fr       */
+/*   Updated: 2026/04/05 21:26:30 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "webserv.hpp"
 #include "utils.hpp"
 
-void	ft_error(const std::string& str) {
-	std::cerr << str << std::endl;
+void	ft_crash(const std::string& str) {
+	std::cerr << "[CRASH]" << str << std::endl << strerror(errno) << std::endl;
 	exit(1);
 }
 
-void	ft_crash(const std::string& str, int exit_code) {
-	std::cerr << "[CRASH]" << str << strerror(errno) << std::endl;;
-	exit(exit_code);
+void	ft_error(const std::string& str) {
+	std::cerr << "[ERROR]" << str << std::endl;
+	return ;
 }
 
-void logs(const std::string& str) {
+void ft_logs(const std::string& str) {
 	std::cout << "[LOGS] [" + str + "]" << std::endl;
+	return ;
 }
 
-void warning(void) {
+void ft_warning(void) {
 	std::cout << "[WARNING] " << strerror(errno) << std::endl;
+	return ;
+}
+
+void ft_free_config(struct config* conf) {
+	if (conf->locationConfig)
+		delete[] conf->locationConfig;
+	delete[] conf;
+	return ;
 }
